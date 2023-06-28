ca1bf371c4e1a2f03043da9a8d6286a9bcc0e789
bodya <bodyanka229@gmail.com>
2023-06-28 23:29:33 +0300

    init commit

---
+++ src/main/java/com/example/glovohomework/GlovoHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.glovohomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlovoHomeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlovoHomeWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/glovohomework/GlovoHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glovohomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlovoHomeWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
